94c48315a129b0e2bf5df77eae5cd4f788bda960
Stephen Adeniji <praiseadeniji2017@gmail.com>
2025-02-24 07:00:16 +0100

    Update Header.tsx

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,5 +1,5 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
-import { EvilIcons } from "@expo/vector-icons";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import { router } from "expo-router";
 
 interface HeaderProps {
@@ -14,11 +14,11 @@ const Header: React.FC<HeaderProps> = ({
   onBackPress,
 }) => {
   return (
-    <View className="w-full pt-5 pb-3 bg-white">
+    <View className="w-full pb-3 bg-white">
       <View className="flex-row items-center gap-4">
         {showBack && (
           <TouchableOpacity onPress={onBackPress || (() => router.back())}>
-            <EvilIcons name="chevron-left" size={36} color="#98A2B3" />
+            <Feather name="chevron-left" size={28} color="#98A2B3" />
           </TouchableOpacity>
         )}
         <Text className="text-2xl text-neutral-900 font-semibold">{title}</Text>
